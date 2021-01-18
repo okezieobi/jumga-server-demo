f025df0e08cc2f62b669033529f4dd7cc094a9b9
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-01-18 20:19:21 +0100

    image upload added

--- src/validations/index.js
+++ src/validations/index.js
@@ -2,12 +2,20 @@ import { validationResult, checkSchema } from 'express-validator';
 
 import UserSchema from './user';
 import EntitySchema from './entity';
+// import fileUpload from '../utils/fileUpload';
 
 const handleValidationErr = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) next();
   else next({ messages: errors.array(), status: 400 });
 };
+
+/*
+const handleFileUploadErr = ({ file }, res, next) => {
+  if (file) next();
+  else next({ message: 'File upload is required', status: 400 });
+};
+*/
 
 const userSchema = new UserSchema(checkSchema);
 const entitySchema = new EntitySchema(checkSchema);
